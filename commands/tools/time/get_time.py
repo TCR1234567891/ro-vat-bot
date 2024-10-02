4e6ad566e7df99302e5a8544288817112464b485
Tudor Rotar <tudorcrotar@gmail.com>
2024-10-02 22:10:23 +0400

    Time Command Fixes

--- commands/tools/time/get_time.py
+++ commands/tools/time/get_time.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def get_time(seconds):
-    mins, secs = divmod(seconds, 60)
+def get_time(secs):
+    mins, secs = divmod(secs, 60)
     hrs, mins = divmod(mins, 60)
     return f"{int(hrs)} hrs, {int(mins)} mins, {secs:.2f} secs"
--- commands/tools/time/get_time_abv.py
+++ commands/tools/time/get_time_abv.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def get_time_abv(seconds):
-    mins, secs = divmod(seconds, 60)
+def get_time_abv(secs):
+    mins, secs = divmod(secs, 60)
     hrs, mins = divmod(mins, 60)
     return f"{int(hrs)}h {int(mins)}m {secs:.2f}s"
